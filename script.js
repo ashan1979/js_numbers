dfee75bd2ee9b3bad197a8b5585e54723cd51b98
ashan <indrajee79@gmail.com>
2023-01-18 23:10:54 +0530

    js

---
+++ script.js
@@ -0,0 +1,14 @@
+const items = [...document.querySelectorAll('.number')];
+
+const updateCount = (el) => {
+    const value = parseInt(el.dataset.value);
+    const increment =  Math.ceil(value / 1000);
+    // const increment = 1;
+    let initialValue = 0;
+
+    const increaseCount =  setInterval(() => {
+        initialValue += increment;
+
+        
+    })
+}
